74d107e944589cb05135bdf851c991f7c3192e27
ddsr9549 <ddsr9549@leeds.ac.uk>
2026-05-13 20:38:13 +0000

    stm

--- Core/Src/stm32l4xx_hal_msp.c
+++ Core/Src/stm32l4xx_hal_msp.c
@@ -87,14 +87,10 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
   {
     __HAL_RCC_SPI2_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**SPI2 GPIO Configuration
-    PB13  ------> SPI2_SCK
-    PB15  ------> SPI2_MOSI
-    */
-    GPIO_InitStruct.Pin   = GPIO_PIN_13|GPIO_PIN_15;
-    GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull  = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Pin       = GPIO_PIN_13|GPIO_PIN_15;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_NOPULL;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
   }
